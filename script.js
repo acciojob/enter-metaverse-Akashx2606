c22f54aa29bb7e5b9023ffdd92a94b76ca48d9e5
Akashx2606 <137022133+Akashx2606@users.noreply.github.com>
2025-04-28 16:08:21 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,5 +1,5 @@
 //your JS code here. If required.
 function enterBtn(){
 const  change = document.getElementById('status');
-	change.innerHTML  = '<h1>' + 'Entered Metaverse' + '</h1>';
+	change.innerHTML  = '<h1>'+'Entered Metaverse'+'</h1>';
 }
