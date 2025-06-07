86562a1f2916a160b3975753f95722a74314672c
Akashx2606 <137022133+Akashx2606@users.noreply.github.com>
2025-06-07 09:36:23 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,5 +1,10 @@
 //your JS code here. If required.
-function enterBtn(){
-const  change = document.getElementById('enterBtn');
-	change.innerHTML  = '<h1>Entered Metaverse</h1>';
-}
+  let enter = document.getElementById("status");
+  let enterbutton = document.getElementById("enterBtn");
+  enterbutton.addEventListener("click", function(){
+	  enter.innerHTML = "<h1>Entered Metaverse</h1>";
+  })
+   
+
+
+
